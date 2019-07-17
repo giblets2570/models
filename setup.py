cd0ffc7b6f533019b5d9b28c2082247d275495c1
Tom Murray <tomkeohanemurray@gmail.com>
2019-07-18 00:00:55 +0100

    version 3

--- setup.py
+++ setup.py
@@ -58,6 +58,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/audreyr/nesta_score_label',
-    version='0.2.0',
+    version='0.3.0',
     zip_safe=False,
 )
